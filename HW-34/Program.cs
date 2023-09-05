3400d6cbd758ca36b551ad2848193f1f17d15873
Rendom-lex <140806354+Rendom-lex@users.noreply.github.com>
2023-09-05 17:28:51 +0300

    Add files via upload

---
+++ HW-34/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Размер массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Рабочий массив: ");
+PrintArray(numbers);
+ArrayChet();
+
+void ArrayChet()
+{
+    int count = 0;
+    for (int z = 0; z < numbers.Length; z++)
+        if (numbers[z] % 2 == 0)
+            count++;
+    Console.WriteLine($"Массив из {numbers.Length} чисел. В нём {count} чётных чисел");
+}
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(100, 1000);
+        }
+}
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
